6a007190c0381a5b85feb5cd3152ffa79fd07de2
ah1062 <ah1062@exeter.ac.uk>
2023-05-09 12:24:09 +0100

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+
+data Player = Player {
+  playerPosition :: Point
+                     }
 
 window :: Display
 window = InWindow "haskell-game" (500, 500) (50, 50)
@@ -8,10 +13,21 @@ window = InWindow "haskell-game" (500, 500) (50, 50)
 bg :: Color
 bg = light black
 
-drawing :: Picture
-drawing = pictures []
+drawing :: Player -> Picture
+drawing p = pictures [ uncurry translate (playerPosition p) $ color green $ circleSolid 10  ]
 
+handleKeyPress :: Event -> Player -> Player
+handleKeyPress (EventKey (Char 'w') Down _ _) player = Player ((fst $ playerPosition player),      (snd $ playerPosition player) + 10)
+handleKeyPress (EventKey (Char 'a') Down _ _) player = Player ((fst $ playerPosition player) - 10, (snd $ playerPosition player))
+handleKeyPress (EventKey (Char 's') Down _ _) player = Player ((fst $ playerPosition player),      (snd $ playerPosition player) - 10)
+handleKeyPress (EventKey (Char 'd') Down _ _) player = Player ((fst $ playerPosition player) + 10, (snd $ playerPosition player))
+handleKeyPress _ player                           = player
+
+timestep :: Float -> Player -> Player
+timestep t player = player
 
 main :: IO ()
 main = do
-  display window bg drawing
+  let player = Player (0, 0)
+  
+  play window bg 60 player drawing handleKeyPress timestep
