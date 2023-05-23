3ebf45331fbeecceb2ec462dea354cc44afe9c4a
ah1062 <95500509+ah1062@users.noreply.github.com>
2023-05-23 13:14:24 +0100

    Line Drawing

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Data.Char (toUpper)
 import Data.Maybe
 
 import Graphics.Gloss
+import Graphics.Gloss.Geometry.Line
 import Graphics.Gloss.Interface.IO.Game
 
 data World = World {
@@ -20,6 +21,12 @@ data Player = Player {
 translateP :: Point -> Point -> Point
 translateP (x,y) (x2,y2) = (x+x2, y+y2)
 
+pairToList :: (a, a) -> [a]
+pairToList (x,y) = [x, y]
+ 
+getBoundsSqCorners :: Vector -> [(Point, Point)]
+getBoundsSqCorners (x,y) = [((-x,y), (x,y)), ((x,y), (x,-y)), ((x,-y), (-x,-y)), ((-x,-y), (-x,y))]
+
 window :: Display
 window = InWindow "haskell-game" (600, 600) (50, 50)
 
@@ -29,32 +36,31 @@ bg = light black
 fps :: Int
 fps = 30
 
-initialState :: Player 
-initialState = Player red (0, 0) Nothing
+initialState :: World 
+initialState = World (Player red (0, 0) Nothing) (250, 250)
 
-drawing :: Player -> IO Picture
-drawing p = return $ pictures [ 
+drawing :: World -> IO Picture
+drawing (World p (wbx,wby)) = return $ pictures [ 
                                  uncurry translate (playerPosition p) $ color (playerColor p) $ circleSolid 10,
-                                 color (dark white) $ pictures $ map line [ 
-                                    [(-250, 250), (250, 250)], [(250, 250), (250, -250)], [(250, -250), (-250, -250)], [(-250,-250), (-250, 250)] 
-                                                              ]
-                              ]
+                                 color (dark white) $ pictures $ map (line . pairToList) (getBoundsSqCorners (wbx, wby)),
+                                 pictures [color (dark white) $ line [x, closestPointOnLine p1 p2 x] | let x = playerPosition p, i <- getBoundsSqCorners (wbx,wby), let (p1, p2) = i ]                                                           
+                                                ]
 
-handleEvent :: Event -> Player -> IO Player
-handleEvent (EventKey (Char c) Down _ _) p
-  | toUpper c == 'W' = return $ Player (playerColor p) (translateP (0, 10)  (playerPosition p)) (Just (Char c))
-  | toUpper c == 'S' = return $ Player (playerColor p) (translateP (0, -10) (playerPosition p)) (Just (Char c))
-  | toUpper c == 'D' = return $ Player (playerColor p) (translateP (10, 0)  (playerPosition p)) (Just (Char c))
-  | toUpper c == 'A' = return $ Player (playerColor p) (translateP (-10, 0) (playerPosition p)) (Just (Char c))
-handleEvent (EventKey (Char c) Up _ _) p = return $ Player (playerColor p) (playerPosition p) Nothing
+handleEvent :: Event -> World -> IO World
+handleEvent (EventKey (Char c) Down _ _) (World p wb)
+  | toUpper c == 'W' = return $ World (Player (playerColor p) (translateP (0, 10)  (playerPosition p)) (Just (Char c))) wb
+  | toUpper c == 'S' = return $ World (Player (playerColor p) (translateP (0, -10) (playerPosition p)) (Just (Char c))) wb
+  | toUpper c == 'D' = return $ World (Player (playerColor p) (translateP (10, 0)  (playerPosition p)) (Just (Char c))) wb
+  | toUpper c == 'A' = return $ World (Player (playerColor p) (translateP (-10, 0) (playerPosition p)) (Just (Char c))) wb
+handleEvent (EventKey (Char c) Up _ _) (World p wb) = return $ World (Player (playerColor p) (playerPosition p) Nothing) wb
 handleEvent _ p = return p
 
-timestep :: Float -> Player -> IO Player
-timestep _ p = 
+timestep :: Float -> World -> IO World
+timestep _ (World p wb) = 
   case playerKey p of
     Just key -> do
-      handleEvent (EventKey key Down (Modifiers Up Up Up) (0,0)) p
-    Nothing -> return p
+      handleEvent (EventKey key Down (Modifiers Up Up Up) (0,0)) (World p wb)
+    Nothing -> return $ World p wb
 
 main :: IO ()
 main = playIO window bg fps initialState drawing handleEvent timestep
